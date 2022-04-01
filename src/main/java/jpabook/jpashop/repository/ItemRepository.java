bb0a356935082a1f11421d0ceb198b670b587bef
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-04-02 03:42:16 +0900

    [chore] merge 관련 내용 주석 추가

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -19,6 +19,11 @@ public class ItemRepository {
             em.persist(item);
         else
             em.merge(item); // 수정 목적으로 온 save
+        // 수정 목적으로 인자에 들어온 item은 영속 컨텍스트에 존재 X -> 준영속 엔티티
+        // 따라서 이를 영속 컨텍스트로 넘겨준 뒤 변경해야하는데.. 두가지 방법존재
+            // 1) em.find(item.getId()) 를 사용해 다시 조회한 후 데이터를 수정 -> Dirty Checking 할 수 있게 됨
+            // 2) merge를 사용하여 영속 컨텍스트에 집어넣는다.
+        // but! merge 기능은 위험함. -> why? -> 모든 속성이 변경되기 때문에 병합시 값이 없으면 null 위험 존재.
     }
 
     // 하나 조회
