d6c84fba3af3f9878058aede7f5ae93c8e27d4f5
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-04-02 02:58:02 +0900

    [chore] typo 수정

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -43,7 +43,7 @@ public class MemberRepository {
 
     // 이름으로 찾기
     public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name := name", Member.class)
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
