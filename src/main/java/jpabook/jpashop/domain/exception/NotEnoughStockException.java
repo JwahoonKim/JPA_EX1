93ba1a6eb4d66d6ef3e524486d6377f54b71a014
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-04-03 04:14:44 +0900

    [chore] NotEnoughStockException.java 폴더 이동

--- src/main/java/jpabook/jpashop/domain/exception/NotEnoughStockException.java
+++
@@ -1,6 +0,0 @@
-package jpabook.jpashop.domain.exception;
-
-public class NotEnoughStockException extends RuntimeException {
-    public NotEnoughStockException() {}
-    public NotEnoughStockException(String message) { super(message); }
-}
